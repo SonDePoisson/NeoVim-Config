b341572407ae6977339b63ff7b2ecc031f855566
Clement Poisson <cpoisson@nimbl-bot.com>
2025-06-04 14:34:02 +0200

    Add tab shortcut to validate copilot suggestion

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,6 +3,13 @@ return {
 	cmd = "Copilot",
 	event = "InsertEnter",
 	config = function()
-		require("copilot").setup({})
+		require("copilot").setup({
+			suggestion = {
+				auto_trigger = true,
+				keymap = {
+					accept = "<Tab>",
+				},
+			},
+		})
 	end,
 }
